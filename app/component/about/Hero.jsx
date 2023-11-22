82e2355f0b7258984e72d3b849f5ea112b10fee1
Aswin-art <aswinarung2@gmail.com>
2023-11-22 19:14:04 +0700

    Fix hero about

--- app/component/about/Hero.jsx
+++ app/component/about/Hero.jsx
@@ -1,28 +1,36 @@
 'use client'
 import { Lexend_Peta } from "next/font/google";
-import { TypeAnimation } from 'react-type-animation';
+import { useEffect, useState } from "react";
+import ReactTextTransition, { presets } from "react-text-transition";
 
+const TEXTS = ["MARKAS", "RUMAH", "STUDIO"];
 const lexend_peta = Lexend_Peta({ subsets: ['latin'], weight: ['400', '700'] })
 
 const Hero = () => {
+    const [index, setIndex] = useState(0);
 
+    useEffect(() => {
+        const intervalId = setInterval(() =>
+            setIndex((index) => index + 1), 2000);
+
+        return () => {
+            clearTimeout(intervalId);
+        }
+    }, []);
     return (
         <div className="  h-screen bg-cover bg-[url('/static/img/hero.png')]">
             <div className="w-full h-full flex flex-col gap-8 md:gap-16 justify-center items-center 
              bg-black/80 backdrop-brightness-75 text-white">
-                <TypeAnimation
-                    sequence={[
-                        'Ini cerita tentang MARKAS kita...',
-                        1000,
-                        'Ini cerita tentang RUMAH kita...',
-                        1000,
-                        'Ini cerita tentang LINGKUNGAN kita...',
-                        1000,
-                    ]}
-                    speed={35}
-                    repeat={Infinity}
-                    className="font-light text-xl md:text-2xl lg:text-3xl xl:text-6xl text-center"
-                />
+                <span className="font-light text-xl md:text-2xl xl:text-4xl text-center">
+                    Ini cerita tentang
+                    <ReactTextTransition
+                        springConfig={presets.stiff}
+                        direction="up"
+                        inline
+                        className="ml-2 text-primary font-bold">
+                        {TEXTS[index % TEXTS.length]}
+                    </ReactTextTransition> kita...
+                </span>
                 <span className="font-light text-xl md:text-2xl text-center">
 
                 </span>
